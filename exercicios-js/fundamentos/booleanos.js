48882f856b71a6a2e5403d98548aad87e5f1906f
Yanni Nadur <yanninadur@gmail.com>
2020-05-25 11:23:43 -0300

    Primeiro upload, teste

---
+++ exercicios-js/fundamentos/booleanos.js
@@ -0,0 +1,42 @@
+let isAtivo = false;
+console.log(isAtivo);
+
+isAtivo = true;
+console.log(isAtivo);
+
+isAtivo = 1;
+console.log(!!isAtivo);
+
+isAtivo = 0;
+console.log(!!isAtivo);
+
+console.log("\nos verdadeiros...\n");
+console.log(!!3);
+console.log(!!-1);
+console.log(!!"  ");
+console.log(!!"texto");
+console.log(!![]);
+console.log(!!{});
+console.log(!!Infinity);
+console.log(!!(isAtivo = true));
+
+console.log("\nos falsos...\n");
+
+console.log(!!0);
+console.log(!!"");
+console.log(!!null);
+console.log(!!NaN);
+console.log(!!undefined);
+console.log(!!(isAtivo = false));
+
+console.log("\npra finalizar....\n");
+
+console.log(!!("" || null || 0 || "epa"));
+console.log(("" || null || 0 || "epa"));
+
+let nome = "Lucas";
+
+console.log(nome || "Desconhecido");
+
+
+
